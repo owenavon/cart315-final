e9b871dce6e2291b6acc2154c7a3bfedae18066c
Owen Avon <89999663+owenavon@users.noreply.github.com>
2023-03-26 23:41:36 -0400

    Improved / more realistic player controller
    - Player can no longer ski up the hill.
    
    - Character turns as user presses on keyboard inputs.
    
    - Character turns faster with longer press of arrow key. This simulates a human pushing their weight onto the inside edge of the ski to turn more aggressively.

--- Assets/playerController.cs
+++ Assets/playerController.cs
@@ -4,8 +4,37 @@ using UnityEngine;
 public class playerController : MonoBehaviour
 {
     public Rigidbody rig; // A referene to our Rigidbody component
-    public float moveForce; // Force applied when we move left or right
+    private float downForce = 180; // Force applied when we move left or right
+    private float upForce = -100;
+    private float maxSpeed = 20;
+    private float speed = 1;
+    private float rotationSpeed = 180;
+
     // public float moveSpeed = 500f;
+
+    private void Update()
+    {
+        float currentSpeed = rig.velocity.magnitude;
+
+        Debug.Log(currentSpeed);
+
+        if (currentSpeed >= maxSpeed)
+        {
+            rig.velocity = rig.velocity.normalized * maxSpeed;
+        }
+
+
+        if (currentSpeed <= 1)
+        {
+            upForce = 0;
+            downForce = 0;
+        }
+        else
+        {
+            upForce = -50;
+            downForce = 90;
+        }
+    }
 
     void FixedUpdate()
     {
@@ -15,13 +44,26 @@ public class playerController : MonoBehaviour
         // Get the Vertical input
         float verticalInput = Input.GetAxis("Vertical");
 
-        //if (Input.GetKey("down"))
-        //{
-            //GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -5);
-        //}
-
         // Add force based on our input
-        rig.AddForce(Vector3.right * horizontalInput * moveForce);
-        rig.AddForce(Vector3.forward * verticalInput * moveForce);
+        rig.AddForce(Vector3.right * horizontalInput * downForce);
+        
+        rig.AddForce(Vector3.left * verticalInput * upForce);
+        rig.AddForce(Vector3.right * verticalInput * upForce);
+
+
+
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);            
+        }
+
+        // Debug.Log(Vector3.right * horizontalInput * moveForce);
+        // Debug.Log(Vector3.forward * verticalInput * upForce);
     }
 }
